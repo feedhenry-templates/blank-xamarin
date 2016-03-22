bde381d9e2b0754f5d45767e07e895eebe66f229
Erik Jan de Wit <erikjan.dewit@gmail.com>
2016-03-22 16:28:29 +0100

    iOS version

---
+++ blank-xamarin-ios/ViewController.cs
@@ -0,0 +1,35 @@
+﻿using System;
+
+using UIKit;
+using FHSDK;
+
+namespace blank_xamarin_ios
+{
+	public partial class ViewController : UIViewController
+	{
+		public ViewController (IntPtr handle) : base (handle)
+		{
+		}
+
+		public async override void ViewDidLoad ()
+		{
+			base.ViewDidLoad ();
+
+			try 
+			{
+				var initTask = await FHClient.Init();
+				if (!initTask)
+				{
+					StatusLabel.Text = "FH init successful";
+				}
+			}
+			catch(Exception e)
+			{
+				StatusLabel.Text = "FH init in error";
+				Console.WriteLine("Error {0}", e);
+			}
+		}
+	}
+}
+
+
